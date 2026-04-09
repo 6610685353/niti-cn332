e8b51d49bd61a59736687c1d45e82801dad55f12
Thanawan Phongphaew <6610685171@student.tu.ac.th>
2026-04-09 23:54:30 +0700

    feat: integrate Firebase into juristic, technician, and mobile applications

--- smart_niti_app/juristic_app/lib/main.dart
+++ smart_niti_app/juristic_app/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:juristic_app/core/constants/app_colors.dart';
 import 'package:juristic_app/route.dart';
-import 'package:firebase_core/firebase_core.dart'; 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
