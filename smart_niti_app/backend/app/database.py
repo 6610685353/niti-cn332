3a1b1c1b0430f5a31628360f0eb7a530c5c16cdf
Punnawat Namkum <6610685247@student.tu.ac.th>
2026-05-17 23:50:39 +0700

    Link backend to Supabase and update requirements.txt

--- smart_niti_app/backend/app/database.py
+++ smart_niti_app/backend/app/database.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://dev:password@127.0.0.1:5500/mydb"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
