755c07407f0d13cb7800f6ae51f28cf83167be9b
Netchanok Yindee <tungpang.yindee2548@gmail.com>
2026-05-24 14:12:01 +0700

    fix config

--- smart_niti_app/mobile_app/lib/resident/core/app_config.dart
+++ smart_niti_app/mobile_app/lib/resident/core/app_config.dart
@@ -3,5 +3,6 @@ class AppConfig {
   // ถ้ารันบน Android Emulator ใช้ 10.0.2.2 แทน localhost
   // ถ้ารันบน iOS Simulator หรือ Physical Device ใช้ IP ของเครื่อง
   // static const String baseUrl = 'http://10.0.2.2:8000';
-  static const String baseUrl = 'http://192.168.1.35:8000';
+  static const String baseUrl =
+      'https://smart-niti-backend-3d8k8.ondigitalocean.app';
 }
